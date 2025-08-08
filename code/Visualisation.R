1014dbb6cd53819a3d3b2955c75b136e7acc2ed7
lcma666 <67261471+lcma666@users.noreply.github.com>
2025-08-08 13:09:13 +0100

    Update Visualisation.R

--- code/Visualisation.R
+++ code/Visualisation.R
@@ -1,1 +1,9 @@
 # code for visualising 
+library (terra)
+library (imageRy)
+#to install github packages
+library (devtools)
+devtools::install_github ("ducchiorocchin/imageRy")
+
+#improting data
+im.lis()
