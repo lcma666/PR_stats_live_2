503b3f6f09ff66ffcd070f66ba8a57e82fa6f894
lcma666 <67261471+lcma666@users.noreply.github.com>
2025-08-20 10:31:56 +0100

    Update Visualisation.R

--- code/Visualisation.R
+++ code/Visualisation.R
@@ -13,3 +13,56 @@ im.list()
 
 # importing the data
 b2 <- im.import("sentinel.dolomites.b2.tif")
+
+cl <- colorRampPalette(c("cyan","chocolate","chartreuse"))(1000)
+plot(b2, col=cl)
+
+cl <- colorRampPalette(c("cyan","chocolate","chartreuse"))(100)
+plot(b2, col=cl)
+
+cl <- colorRampPalette(c("cyan","chocolate","chartreuse"))(3)
+plot(b2, col=cl)
+
+# multiframe
+par(mfrow=c(1,3))
+
+cl <- colorRampPalette(c("cyan","chocolate","chartreuse"))(1000)
+plot(b2, col=cl)
+
+cl <- colorRampPalette(c("cyan","chocolate","chartreuse"))(100)
+plot(b2, col=cl)
+
+cl <- colorRampPalette(c("cyan","chocolate","chartreuse"))(3)
+plot(b2, col=cl)
+
+# replot b2 with a different color ramp palette
+https://sites.stat.columbia.edu/tzheng/files/Rcolor.pdf
+
+dev.off()
+cl <- colorRampPalette(c("cyan","chocolate","chartreuse","hotpink","maroon4","slategray4"))(100)
+plot(b2, col=cl)
+
+setwd("~/Desktop")
+# setwd("C://")
+pdf("mygraph.pdf")
+cl <- colorRampPalette(c("cyan","chocolate","chartreuse","hotpink","maroon4","slategray4"))(100)
+plot(b2, col=cl)
+dev.off()
+
+png("mygraph.png")
+cl <- colorRampPalette(c("cyan","chocolate","chartreuse","hotpink","maroon4","slategray4"))(100)
+plot(b2, col=cl)
+dev.off()
+
+png("mygraph.png")
+par(mfrow=c(2,2))
+cl <- colorRampPalette(c("cyan","chocolate","chartreuse","hotpink","maroon4","slategray4"))(100)
+plot(b2, col=cl)
+cl <- colorRampPalette(c("chartreuse","firebrick","cyan"))(100)
+plot(b2, col=cl)
+cl <- colorRampPalette(c("black","grey","light grey"))(100)
+plot(b2, col=cl)
+cl <- colorRampPalette(c("seagreen","seagreen1","seagreen2","seagreen3","seagreen4"))(100)
+plot(b2, col=cl)
+dev.off()
+
